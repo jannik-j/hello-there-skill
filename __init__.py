240f8378c05e9da6da99a790f9a02fc6f2404e2b
Jannik Jacobsen <jannik.jacobsen@tuhh.de>
2021-06-26 20:01:23 +0200

    rename files, add support for german

--- __init__.py
+++ __init__.py
@@ -5,9 +5,9 @@ class HelloThere(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('there.hello.intent')
+    @intent_file_handler('hello.there.intent')
     def handle_there_hello(self, message):
-        self.speak_dialog('there.hello')
+        self.speak_dialog('hello.there')
 
 
 def create_skill():
